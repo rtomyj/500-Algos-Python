43dc03976885837ca740a8695918b591f743e198
Javi Gomez <rtomyj@gmail.com>
2019-02-19 22:01:35 -0600

    completed challenge four

---
+++ Four/four.py
@@ -0,0 +1,16 @@
+items = [1, 0, 1, 0, 1, 0, 0, 1]
+leftMostValue = 1	# determines whether sorted binray list starts with a 1 or 0
+leftMostItems, rightMostItems = [], []
+
+
+for value in items:
+	if value == leftMostValue:
+		leftMostItems.append(value)
+	else:
+		rightMostItems.append(value)
+
+'''
+	extends() is faster than iterating and adding the values individually in test cases
+'''
+leftMostItems.extend(rightMostItems)
+print(leftMostItems)
