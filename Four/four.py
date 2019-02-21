85cdcf911f39b1a20d968801809e304372ba0771
Javi Gomez <rtomyj@gmail.com>
2019-02-20 19:56:35 -0600

    new algo

--- Four/four.py
+++ Four/four.py
@@ -1,5 +1,6 @@
-items = [1, 0, 1, 0, 1, 0, 0, 1]
+items = [1, 0, 1, 0, 1, 0, 0, 1] 
 leftMostValue = 1	# determines whether sorted binray list starts with a 1 or 0
+'''
 leftMostItems, rightMostItems = [], []
 
 
@@ -10,7 +11,21 @@ for value in items:
 		rightMostItems.append(value)
 
 '''
+'''
 	extends() is faster than iterating and adding the values individually in test cases
+'''
 '''
 leftMostItems.extend(rightMostItems)
 print(leftMostItems)
+'''
+
+leftMostValue = True
+total = 0
+
+for value in items:
+	if value == leftMostValue:
+		total += 1
+
+sortedItems = [int(leftMostValue)] * total
+sortedItems.extend([int(not leftMostValue)] * (len(items) - total))
+print(sortedItems)
