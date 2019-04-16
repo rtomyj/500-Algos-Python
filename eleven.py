73af7ed2c8c0dbb3e9cd166db9dd47e971ac3ac9
Javi Gomez <rtomyj@gmail.com>
2019-04-15 21:26:06 -0500

    documentation

--- eleven.py
+++ eleven.py
@@ -1,35 +1,72 @@
+from sys import path
+path.append('libs')
+from lists import swap_elements
+
+'''
+	Main functionality.
+	Completes algorithm 11.
+'''
 x = [0, 2, 0, 3, 0, 5, 6, 0, 0]
 y = [1, 8, 9, 10, 15]
 
-emptyCells = []
-xInd = 0
-yInd = 0
+emptyCells = []	# indices of all 0's (empty cells) found in x
+xInd = 0; yInd = 0	# which indexes in lists (x, y) algorithm is currently comparing
 
+'''
+	Compares all elements of x with elements of y.
+	Terminates once all elements of y have been compared and merged into x
+'''
 while yInd < len(y):
 	if xInd == len(x):
+		'''
+			Condition is met once all elements of x have been traversed.
+			This happens when the y list has elements greater than all elements of x. All remaining elements of y are added 
+			to unused indexes of x.
+		'''
 		x[emptyCells[0]] = y[yInd]
-		emptyCells = emptyCells[1:]
+		emptyCells = emptyCells[1:]	# removes used emptyCell from list
 		yInd += 1
 		continue
 
+	'''
+		Current elements.
+	'''
 	xEle = x[xInd]
 	yEle = y[yInd]
 
 	if xEle == 0:
+		'''
+			Unused spot in x is found. 
+			Must be traversed over.
+		'''
 		emptyCells.append(xInd)
 		xInd += 1
 	else:
+		'''
+			Comparison will be done to element of x with element of y.
+		'''
 		if xEle > yEle:
 			if len(emptyCells) > 0:
+				'''
+					If there is an emptyCell in x then put the y element into the left most empty cell.
+				'''
 				x[emptyCells[0]] = yEle
-				emptyCells = emptyCells[1:]
+				emptyCells = emptyCells[1:]	# removes used emptyCell from list
 				yInd += 1
 			else:
-				x[xInd] = yEle
-				y[yInd] = xEle
+				'''
+					If there is no emptyCell then swap the x element with the y element.
+					After the swap, the y element is the greater of the two and we are ok to increment the x index.
+				'''
+				swap_elements(x, xInd, yInd, y)
 				xInd += 1
 		else:
 			if len(emptyCells) > 0:
+				'''
+					The x value is less than the y value meaning we still need to find a spot for the y element (don't increment y).
+					If there is an emptyCell then the x element needs to go to the left most emptyCell.
+					Otherwise, x is already sorted and no swap is needed but the x index needs to be incremented.
+				'''
 				x[emptyCells[0]] = xEle
 				x[xInd] = 0
 				emptyCells = emptyCells[1:]
--- nine.py
+++ nine.py
@@ -1,31 +1,54 @@
-items = [0, 1, 2, 2, 1, 0, 0, 2, 0, 1, 1, 0]
-
 from sys import path
 path.append('libs')
 from lists import swap_elements
 
+'''
+	Sorts an array partially by swapping a desired element with an index not yet sorted.
+	Ideally to sort an array this method will be called multiple times.
+	If there are n possible values for an element then this method should be called n-1 times.
+'''
 def swap_sort(items, lookingFor, unsortedInd):
 	for ind in range(unsortedInd, len(items)):
+		'''
+			Swaps current value with the value at the unsorted index if current value is the current wanted value.
+		'''
 		if lookingFor == items[ind]:
 			swap_elements(items, unsortedInd, ind)
-			unsortedInd += 1
+			unsortedInd += 1	# array is partially sorted, increment index to get to new unsorted index
 
 	return unsortedInd
 
+
+'''
+	Sorts an array that contains 0's, 1's and 2's.
+	Sorting should use constant space and be done in linear time.
+'''
 def sort(items, asc=True):
+	'''
+		Default values for ascending sort.
+	'''
 	if asc:
 		leftMostValue = 0
 		increment = 1
 	else:
+		'''
+			Changes values for descending sort.
+		'''
 		leftMostValue = 2
 		increment = -1
 
-	unsortedInd = swap_sort(items, leftMostValue, 0)
-	swap_sort(items, leftMostValue + increment, unsortedInd)
+	unsortedInd = swap_sort(items, leftMostValue, 0)	# partially sorts array (either 0 (asc) or 2 (desc) are all sorted)
+	swap_sort(items, (leftMostValue + increment), unsortedInd)	# array is sorted
 
-sort(items)
+
+'''
+	Main functionality
+	Completes algorithm 9.
+'''
+items = [0, 1, 2, 2, 1, 0, 0, 2, 0, 1, 1, 0]
+
+sort(items)	# sorts 0's, 1's and 2's in ascending order
 print(items)
 
-sort(items, asc=False)
+sort(items, asc=False)	# sorts 0's, 1's and 2's in descending order
 print(items)
-
